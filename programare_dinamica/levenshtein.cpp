03c5ebb7d8aa3e8dfb0863904e84918304f15a83
Andrei Visoiu <visoiuandrei42@yahoo.ro>
2019-11-25 11:43:37 +0200

    Update levenshtein.cpp

--- programare_dinamica/levenshtein.cpp
+++ programare_dinamica/levenshtein.cpp
@@ -7,7 +7,7 @@ const int N = 100;
 ifstream in("cuvinte.in");
 
 char s[N], d[N];
-int c[N][N],
+int c[N][N], nrOp[N][N],
     n, m;
 
 
@@ -24,12 +24,31 @@ int cmp(int m, int n) {
                             });
 }
 
+int lev(int m, int n) {
+    for(int i = 0; i <= m; i++)
+        nrOp[i][0] = i;
+    for(int i = 0; i <= n; i++)
+        nrOp[0][i] = i;
+
+    for(int i = 1; i <= m; i++)
+        for(int j = 1; j <= n; j++)
+            if(s[i] == d[j])
+                nrOp[i][j] = nrOp[i-1][j-1];
+            else nrOp[i][j] = 1 + min({
+                                        nrOp[i-1][j], /// stergere
+                                        nrOp[i-1][j-1], /// inlocuire
+                                        nrOp[i][j-1] /// inserare
+                                      });
+    return nrOp[m][n];
+}
+
 int main()
 {
     in >> s+1 >> d+1;
     m = strlen(s+1);
     n = strlen(d+1);
 
-    cout << cmp(m, n);
+    cout << cmp(m, n) << '\n';
+    cout << lev(m, n);
     return 0;
 }
