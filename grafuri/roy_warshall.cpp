47bf56979dd1f1e26c8263a49085850d9aaef030
Andrei Visoiu <visoiuandrei42@yahoo.ro>
2020-02-17 11:42:48 +0200

    Update roy_warshall.cpp

--- grafuri/roy_warshall.cpp
+++ grafuri/roy_warshall.cpp
@@ -7,31 +7,36 @@ ifstream in("graf.in");
 const int N = 100;
 int n, m;
 
-void show(bool a[][N]) {
-    for(int i = 1; i <= n; i++, cout << '\n')
-        for(int j = 1; j <= n; j++)
-            cout << a[i][j] << ' ';
-}
-
 int main()
 {
     bool a[N][N] = {0};
-    int x, y;
+    int x, y, comp[N] = {0}, nrCompTC = 0;
     in >> n >> m;
 
     while(m--) {
         in >> x >> y;
         a[x][y] = 1;
     }
-    show(a);
 
-    for(int k = 1; k <= n; k++)  { /// k intermediar
+
+    for(int k = 1; k <= n; k++)  /// k intermediar
         for(int i = 1; i <= n; i++)
             for(int j = 1; j <= n; j++)
                 a[i][j] |= (a[i][k] && a[k][j]); /// exista drum i->k si drum k->j sau drum i->j
-        cout << "\nTransformarea: " << k << '\n';
-        show(a);
-    }
+
+    int vf = 1;
+    do {
+        nrCompTC++;
+        comp[vf] = nrCompTC;
+        cout << "Componenta tare conexa nr: " << nrCompTC << ": " << vf << ' ';
+        for(int i = 1; i <= n; i++)
+            if(a[i][vf] && a[i][vf] == a[vf][i] && comp[i] == 0) {
+                comp[i] = nrCompTC;
+                cout << i << ' ';
+            }
+        cout << '\n';
+        while(comp[++vf] != 0 && vf <= n);
+    } while(vf <= n);
 
     return 0;
 }
